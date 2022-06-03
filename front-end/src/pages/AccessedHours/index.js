6c0d60bd71202c411dabaa71af675f1f92cafcea
Gabriel Teixeira <61993394+Gabrieltg7@users.noreply.github.com>
2022-06-03 00:28:53 -0300

    initial commit

--- front-end/src/pages/AccessedHours/index.js
+++ front-end/src/pages/AccessedHours/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
 
 function AccessedHours() {
+  const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 300, pv: 2400, amt: 2400}];
   return (
-        <div className='App'>
-           <h1>Most Accessed Hours</h1>
-        </div>
-  );
+    <center>
+      <LineChart width={600} height={300} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
+      </center>
+    );
 }
 
 export default AccessedHours;
