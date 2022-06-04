def8e20836d62aa2044d1d189d04716047a372d7
Gabriel Teixeira <61993394+Gabrieltg7@users.noreply.github.com>
2022-06-04 18:30:56 -0300

    Integration back/front

--- front-end/src/pages/AccessedHours/index.js
+++ front-end/src/pages/AccessedHours/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import React, { useEffect } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import axios from 'axios';
 
 
 function AccessedHours() {
   const [data, setData] = React.useState([]);
-  const response = axios.get('http://localhost:5000/api/AccessedHours').then((data) => setData(data))
-  const datas = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 300, pv: 2400, amt: 2400}];
+  useEffect(() => {
+    axios.get('http://localhost:5001/api/access').then((data) => setData(data.data));
+  }, []);
+  const data1 = [];
+ data.map((hour) => {
+  data1.push({name: hour.hours[0].hour, uv: hour.hours[0].ammount})
+  });
+  data1.sort((a, b) => a.name - b.name);
   return (
     <center>
-      <LineChart width={600} height={300} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <LineChart width={600} height={300} data={data1}>
+        <Line type="monotone" dataKey="uv" stroke="#000000" />
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="name" />
         <YAxis />
+        <Tooltip />
       </LineChart>
       </center>
     );
