c055a6599ea38630f4271501de0c6c7422cc352d
Edryan.Maciel <edryan.santos@softwillians.com.br>
2022-06-04 18:36:49 -0300

    Html and css of getUserLocation page

--- front-end/src/pages/Location/index.js
+++ front-end/src/pages/Location/index.js
@@ -15,17 +15,16 @@ function Location() {
       return a.data
     }
     fetchData().then((response) => { setJsonLocation(response) });
-},[]);
-let dictionary = new Object()
-  for(let i = 0; i <jsonLocation.length; i++)
-  { 
+  }, []);
+  let dictionary = new Object()
+  for (let i = 0; i < jsonLocation.length; i++) {
     console.log(jsonLocation[i])
-    if(!dictionary[jsonLocation[i].country]) dictionary[jsonLocation[i].country] = 0
-    dictionary[jsonLocation[i].country] = dictionary[jsonLocation[i].country] +1;
-  }  
+    if (!dictionary[jsonLocation[i].country]) dictionary[jsonLocation[i].country] = 0
+    dictionary[jsonLocation[i].country] = dictionary[jsonLocation[i].country] + 1;
+  }
 
   let graphData = []
-  for(const country in dictionary) {
+  for (const country in dictionary) {
     graphData.push({
       name: country, Quantidade: dictionary[country]
     })
@@ -35,18 +34,22 @@ let dictionary = new Object()
     <div className="App">
     <div className="main">
       <div className="center">
-      <h1 >Localizações de logins por usuários</h1>
-      <BarChart width={600} height={300} data={graphData}>
-      <XAxis dataKey="name" stroke="#8A2BE2" />
-      <YAxis />
-      <Tooltip  wrapperStyle={{ width: 120, backgroundColor: '#ccc' }} />
-      <Legend width={200} wrapperStyle={{ top: 10, right: 10, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-      <Bar dataKey="Quantidade" fill="#8A2BE2" barSize={30} />
-      </BarChart>
+        <div className="header">
+            <h3>Localizações de logins por usuários</h3>
         </div>
+      </div>
+    </div>
+        <div className='barchart'>
+          <BarChart width={1800} height={300} data={graphData} top={500}>
+            <XAxis dataKey="name" stroke="#8A2BE2" />
+            <YAxis />
+            <Tooltip wrapperStyle={{ width: 120, backgroundColor: '#ccc' }} />
+            <Legend width={200} wrapperStyle={{ top: 10, right: 10, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <Bar dataKey="Quantidade" fill="#8A2BE2" barSize={30} />
+          </BarChart>
         </div>
-        </div>
+      </div>
   );
 }
 
