7c8989f987036735d6213916469c0d02f088fa9e
Gabriel Teixeira <61993394+Gabrieltg7@users.noreply.github.com>
2022-06-04 03:35:42 -0300

    Update index.js

--- front-end/src/pages/AccessedHours/index.js
+++ front-end/src/pages/AccessedHours/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import axios from 'axios';
 
 
 function AccessedHours() {
-  const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 300, pv: 2400, amt: 2400}];
+  const [data, setData] = React.useState([]);
+  const response = axios.get('http://localhost:5000/api/AccessedHours').then((data) => setData(data))
+  const datas = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 300, pv: 2400, amt: 2400}];
   return (
     <center>
       <LineChart width={600} height={300} data={data}>
