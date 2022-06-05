f7d67eb1c7e9ffe43caa655a5aca58587327941e
Gabriel Teixeira <61993394+Gabrieltg7@users.noreply.github.com>
2022-06-05 16:05:37 -0300

    Hours (#4)
    * initial commit
    
    * Update index.js
    
    * Integration back/front
    
    * Delete package-lock.json

--- front-end/src/pages/AccessedHours/index.js
+++ front-end/src/pages/AccessedHours/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import axios from 'axios';
+
 
 function AccessedHours() {
+  const [data, setData] = React.useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5001/api/access').then((data) => setData(data.data));
+  }, []);
+  const data1 = [];
+ data.map((hour) => {
+  data1.push({name: hour.hours[0].hour, uv: hour.hours[0].ammount})
+  });
+  data1.sort((a, b) => a.name - b.name);
   return (
-        <div className='App'>
-           <h1>Most Accessed Hours</h1>
-        </div>
-  );
+    <center>
+      <LineChart width={600} height={300} data={data1}>
+        <Line type="monotone" dataKey="uv" stroke="#000000" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+      </center>
+    );
 }
 
 export default AccessedHours;
