342684c61bafbed2a235e17d4e4066721fa2402a
LeticiaSan <leticia.santos74@fatec.sp.gov.br>
2022-06-05 16:37:36 -0300

    Header Component

---
+++ front-end/src/Components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../Styles/Homepage/style.css";
+
+function Header() {
+  <div className="App">
+    <div className="main">
+      <div className="center">
+        <div className="header">
+          <div className="Logo">
+            <h3>Brisk.Administrator</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
+}
+export default Header();
